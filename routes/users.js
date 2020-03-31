8ce7e9c9fb602ed3d3e28e88c16db4675765f1c1
pavan kumar <pavank@demenew.com>
2020-04-01 02:05:11 +0530

    added token code for getusers

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const jwt = require('jsonwebtoken');
 
 const User = require("../public/javascripts/users");
 
@@ -30,16 +31,29 @@ router.post('/checkforuser', (req, res) =>{
   })
 })
 
-router.get('/getUsers', async (req, res) =>{
+router.get('/getUsers', verifyToken, async (req, res) =>{
   User.find().then(doc =>{
-    res.json(doc);
+    jwt.verify(req.token, 'secretkey', (err, usersdata) =>{
+      if(err){
+        res.sendStatus(403);
+      }else{
+        res.json({
+          doc
+        });
+      }
+    })
   });
 });
 
 router.post('/findLoger', async (req, res) =>{
   User.find({username : req.body.username, password: req.body.password})
   .then(doc =>{
-      res.json(doc);
+      jwt.sign({doc}, 'secretkey', (error, token) =>{
+        res.json({
+          token,
+          doc
+        })
+      })
   })
   .catch(error =>{
     console.log(error);
@@ -53,5 +67,20 @@ router.post('/getuser', async(req, res) =>{
   })
   .catch(error => console.error);
 });
+
+
+function verifyToken(req, res, next){
+  console.log(req.headers);
+const bearereHeader = req.headers['authorization'];
+if(typeof bearereHeader !== 'undefined'){
+  const bearer = bearereHeader.split(' ');
+  const bearereToken = bearer[1];
+  req.token = bearereToken;
+  next();
+}else{
+  res.sendStatus(403);
+}
+
+}
 
 module.exports = router;
